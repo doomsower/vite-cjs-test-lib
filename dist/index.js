8c6e40dc2c8098d23a84f498dd1022d30f03ef3c
Konstantin Kuznetsov <K.Kuznetcov@gmail.com>
2021-11-13 20:19:03 +0300

    feat: first commit

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.test = void 0;
+function test() {
+    return "cjs-remote";
+}
+exports.test = test;
+//# sourceMappingURL=index.js.map
